bad8d657e0c7e23639804a7ad6e828d84209cf5d
jsjlewis96 <jsjlewis96@gmail.com>
2019-05-05 18:02:43 +0100

    now using leaflet js

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,14 +1,31 @@
 import * as React from 'react';
 import MapPoint from './MapPoint';
+import * as leaflet from 'leaflet';
 import './Map.css';
+import '../../../node_modules/leaflet/dist/leaflet.css'
 
-export default () => {
-  return (
-    <div className="mapContainer">
-      <div className='map'>
-        <MapPoint x={54} y={24} hovertext='blarg' onClick={() => console.log('clicked')} />
-        <MapPoint x={33} y={63} hovertext='blarg2' onClick={() => console.log('clicked')} />
+const mapImage = require('../../images/metrunui.png');
+
+export default class Map extends React.Component {
+
+  componentDidMount() {
+    const map = leaflet.map('map', {
+      crs: leaflet.CRS.Simple
+    });
+    console.log(mapImage);
+    const bounds = new leaflet.LatLngBounds([[0, 0], [1522, 780]]);
+    const image = leaflet.imageOverlay(mapImage, bounds).addTo(map);
+    map.fitBounds(bounds);
+  }
+
+  render() {
+    return (
+      <div className="mapContainer" >
+        <div className='map' id='map'>
+          {/* <MapPoint x={54} y={24} hovertext='blarg' onClick={() => console.log('clicked')} />
+          <MapPoint x={33} y={63} hovertext='blarg2' onClick={() => console.log('clicked')} /> */}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
