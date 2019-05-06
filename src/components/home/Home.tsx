40aaf469f21c1ac034708f97dc6df78e72ab989e
Matt Rayner <me@mattihew.com>
2019-05-06 11:15:22 +0100

    fixed homepage buttons grid layout

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -3,12 +3,14 @@ import Page from '../common/Page'
 import NavButton from '../common/NavButton';
 
 export default () => {
-  return (
-    <Page title='PROJECT SHADE' tagline='A Digital "Enhancement" for Doronai Nui'>
-      <NavButton text='AKUTANA' href='/map' />
-      <NavButton text='THE CANISTER' href='/canister' />
-      <NavButton text="CHRONICLER'S NOTES" href='/notes' />
-      <NavButton text='RED STAR FORGE' href='https://www.shapeways.com/shops/forge-of-the-red-star' />
-    </Page>
-  );
+    return (
+        <Page title='PROJECT SHADE' tagline='A Digital "Enhancement" for Doronai Nui'>
+            <div>
+                <NavButton text='AKUTANA' href='/map'/>
+                <NavButton text='THE CANISTER' href='/canister'/>
+                <NavButton text="CHRONICLER'S NOTES" href='/notes'/>
+                <NavButton text='RED STAR FORGE' href='https://www.shapeways.com/shops/forge-of-the-red-star'/>
+            </div>
+        </Page>
+    );
 }
